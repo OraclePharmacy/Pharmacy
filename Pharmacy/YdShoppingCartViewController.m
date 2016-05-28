0daf289434042ca4817fa35999785177b3702aad
sk19920518 <350458783@qq.com>
2016-05-28 16:04:39 +0800

    汇总

--- Pharmacy/YdShoppingCartViewController.m
+++ Pharmacy/YdShoppingCartViewController.m
@@ -9,6 +9,7 @@
 #import "YdShoppingCartViewController.h"
 #import "UIImageView+WebCache.h"
 #import "hongdingyi.h"
+#import "Color+Hex.h"
 
 @interface YdShoppingCartViewController ()
 {
@@ -50,7 +51,7 @@
     height = [UIScreen mainScreen].bounds.size.height;
     
     //状态栏名称
-    self.navigationItem.title = @"全部订单";
+    //self.navigationItem.title = @"全部订单";
     //设置self.view背景颜色
     self.view.backgroundColor = [UIColor whiteColor];
     //设置导航栏左按钮
@@ -90,7 +91,7 @@
 }
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(nonnull NSIndexPath *)indexPath
 {
-    return width/3+1;
+    return 100;
 }
 -(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -104,34 +105,58 @@
     
     //药品图片
     UIImageView *image = [[UIImageView alloc]init];
-    image.frame = CGRectMake(10, 5, width/3-10, width/3-10);
+    image.frame = CGRectMake(10, 10, 80, 80);
     [image sd_setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@/hyb/%@",service_host,[yikaishi[indexPath.row]objectForKey:@"picUrl"]]] placeholderImage:[UIImage imageNamed:@"IMG_0800.jpg"]];
     image.layer.cornerRadius=30;
     [cell.contentView addSubview:image];
     //药品名称
     UILabel *name = [[UILabel alloc]init];
-    name.frame = CGRectMake(width/3+10, 0, width/3*2-10, width/3/3-10);
+    name.frame = CGRectMake(100, 10, 200, 20);
     name.text = [NSString stringWithFormat:@"%@",[[yikaishi[indexPath.row] objectForKey:@"product"] objectForKey:@"commonName"]];
-    name.textColor = [UIColor blackColor];
+    name.textColor = [UIColor colorWithHexString:@"323232" alpha:1];
     name.font =[UIFont systemFontOfSize:15];
     //name.backgroundColor = [UIColor redColor];
     [cell.contentView addSubview:name];
-    //药品数量
-    UILabel *number = [[UILabel alloc]init];
-    number.frame = CGRectMake( width/3+10, width/3/3-10, width/3*2-10, width/3/6);
-    number.text = @"预订数量:";
-    number.textColor = [UIColor blackColor];
-    number.font =[UIFont systemFontOfSize:12];
-    //number.backgroundColor = [UIColor yellowColor];
-    [cell.contentView addSubview:number];
+
+    UILabel *yuanjia = [[UILabel alloc]init];
+    yuanjia.frame = CGRectMake(100, 30, 70, 20);
+    yuanjia.text = @"原价:99.8";
+    yuanjia.textColor = [UIColor colorWithHexString:@"646464" alpha:1];
+    yuanjia.font =[UIFont systemFontOfSize:13];
+    [cell.contentView addSubview:yuanjia];
+    
+    UILabel *tejia = [[UILabel alloc]init];
+    tejia.frame = CGRectMake(170, 30, 70, 20);
+    tejia.text = @"特价:99.8";
+    tejia.textColor = [UIColor colorWithHexString:@"646464" alpha:1];
+    tejia.font =[UIFont systemFontOfSize:13];
+    [cell.contentView addSubview:tejia];
+    
+    //生产厂家
+    UILabel *changjia = [[UILabel alloc]init];
+    changjia.frame = CGRectMake(100, 50, 60, 20);
+    changjia.text = @"生产厂家:";
+    changjia.textColor = [UIColor colorWithHexString:@"646464" alpha:1];
+    changjia.font =[UIFont systemFontOfSize:13];
+    //changjia.backgroundColor = [UIColor colorWithHexString:@"646464" alpha:1];
+    [cell.contentView addSubview:changjia];
+    
+    UILabel *vender = [[UILabel alloc]init];
+    vender.frame = CGRectMake( 160, 50, width - 160 , 20);
+    vender.text = [NSString stringWithFormat:@"%@",[[yikaishi[indexPath.row] objectForKey:@"product"] objectForKey:@"manufacturer"]];
+    vender.textColor = [UIColor colorWithHexString:@"646464" alpha:1];
+    vender.font =[UIFont systemFontOfSize:12];
+    [cell.contentView addSubview:vender];
+
+
     //添加数量按钮
     UIButton *add = [[UIButton alloc]init];
-    add.frame = CGRectMake(width/3+5+5, width/3/3+width/3/6, width/3/6, width/3/6);
+    add.frame = CGRectMake(100, 72, 16, 16);
     [add setImage:[UIImage imageNamed:@"IMG_0799.jpg"] forState:UIControlStateNormal];
     [cell.contentView addSubview:add];
     //数量输入框
     num = [[UITextField alloc]init];
-    num.frame = CGRectMake(width/3+5+width/3/6+5, width/3/3+width/3/6, width/3/3, width/3/6);
+    num.frame = CGRectMake(120, 70, 30, 20);
     num.delegate=self;
     num.font = [UIFont systemFontOfSize:13];
     num.layer.borderColor = [[UIColor grayColor] CGColor];
@@ -139,24 +164,12 @@
     num.layer.cornerRadius = 5.0;
     [num addTarget:self action:@selector(NumberLength) forControlEvents:UIControlEventEditingChanged];
     num.text=[NSString stringWithFormat:@"%@",[yikaishi[indexPath.row]objectForKey:@"shuliang"]];
-              [cell.contentView addSubview:num];
-              //删减数量按钮
-              UIButton *reduce = [[UIButton alloc]init];
-              reduce.frame = CGRectMake(width/3+5+width/3/6+width/3/3+5, width/3/3+width/3/6, width/3/6, width/3/6);
-              [reduce setImage:[UIImage imageNamed:@"IMG_0799.jpg"] forState:UIControlStateNormal];
-              [cell.contentView addSubview:reduce];
-              //生产厂家
-              UILabel *vender = [[UILabel alloc]init];
-              vender.frame = CGRectMake( width/3+10, width/3/3+width/3/6+width/3/6+10, width/3*2-10, width/3/6);
-              vender.text = [NSString stringWithFormat:@"%@",[[yikaishi[indexPath.row] objectForKey:@"product"] objectForKey:@"manufacturer"]];
-              vender.textColor = [UIColor blackColor];
-              vender.font =[UIFont systemFontOfSize:12];
-              //number.backgroundColor = [UIColor yellowColor];
-              [cell.contentView addSubview:vender];
-
-    
-    
-    
+    [cell.contentView addSubview:num];
+    //删减数量按钮
+    UIButton *reduce = [[UIButton alloc]init];
+    reduce.frame = CGRectMake(154,72, 16, 16);
+    [reduce setImage:[UIImage imageNamed:@"IMG_0799.jpg"] forState:UIControlStateNormal];
+    [cell.contentView addSubview:reduce];
     
     
     //cell点击不变色
