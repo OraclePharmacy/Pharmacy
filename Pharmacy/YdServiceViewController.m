e0358fa6527824d31049c3f7ad061c2c8aa56367
sk19920518 <350458783@qq.com>
2016-06-06 16:08:43 +0800

    服务添加左按钮

--- Pharmacy/YdServiceViewController.m
+++ Pharmacy/YdServiceViewController.m
@@ -47,6 +47,8 @@
     width = [UIScreen mainScreen].bounds.size.width;
     height = [UIScreen mainScreen].bounds.size.height;
     
+    //设置导航栏左按钮
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"圆角矩形-6@3x.png"] style:UIBarButtonItemStyleDone target:self action:@selector(presentLeftMenuViewController:)];
     
     identifier = @"cell";
     
