28e8d60ac7545005be7b902345ea130789f3db64
sk19920518 <350458783@qq.com>
2016-08-24 15:05:22 +0800

    关于 最后一个自定义cell高度

--- Pharmacy/guanyuViewController.m
+++ Pharmacy/guanyuViewController.m
@@ -8,6 +8,11 @@
 
 #import "guanyuViewController.h"
 #import "Color+Hex.h"
+#import "WarningBox.h"
+#import "AFHTTPSessionManager.h"
+#import "SBJson.h"
+#import "hongdingyi.h"
+#import "lianjie.h"
 
 @interface guanyuViewController ()
 {
@@ -15,6 +20,7 @@
     CGFloat height;
     
     NSArray *shang;
+    NSDictionary *data;
 }
 @property (nonatomic,strong) UITableView *tableview;
 @property (nonatomic, strong) UIView *tableFooterView;
@@ -51,6 +57,67 @@
 
 -(void)jiekou
 {
+    //userID    暂时不用改
+    NSString * userID=@"0";
+    
+    //请求地址   地址不同 必须要改
+    NSString * url =@"/basic/officeUserList";
+    
+    //时间戳
+    NSDate* dat = [NSDate dateWithTimeIntervalSinceNow:0];
+    NSTimeInterval a=[dat timeIntervalSince1970];
+    NSString *timeSp = [NSString stringWithFormat:@"%.0f",a];
+    
+    
+    //将上传对象转换为json格式字符串
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json",@"text/json",@"text/plain",@"text/html", nil];
+    SBJsonWriter *writer = [[SBJsonWriter alloc]init];
+    NSString*officeid;
+    NSUserDefaults*uiwe=  [NSUserDefaults standardUserDefaults];
+    officeid=[NSString stringWithFormat:@"%@",[uiwe objectForKey:@"officeid"]];
+
+    //出入参数：
+    NSDictionary*datadic=[NSDictionary dictionaryWithObjectsAndKeys:@"1",@"pageNo",@"10",@"pageSize",officeid,@"officeId",nil];
+    
+    NSString*jsonstring=[writer stringWithObject:datadic];
+    
+    //获取签名
+    NSString*sign= [lianjie getSign:url :userID :jsonstring :timeSp ];
+    
+    NSString *url1=[NSString stringWithFormat:@"%@%@%@%@",service_host,app_name,api_url,url];
+    
+    //电泳借口需要上传的数据
+    NSDictionary*dic=[NSDictionary dictionaryWithObjectsAndKeys:jsonstring,@"params",appkey, @"appkey",userID,@"userid",sign,@"sign",timeSp,@"timestamp", nil];
+    
+    [manager POST:url1 parameters:dic progress:^(NSProgress * _Nonnull downloadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        [WarningBox warningBoxHide:YES andView:self.view];
+        @try
+        {
+            NSLog(@"responseObject%@",responseObject);
+            if ([[responseObject objectForKey:@"code"] intValue]==0000) {
+                
+                data=[responseObject valueForKey:@"data"];
+                
+                [self.tableview reloadData];
+                
+            }
+        }
+        @catch (NSException * e) {
+            
+            [WarningBox warningBoxModeText:@"请检查你的网络连接!" andView:self.view];
+            
+        }
+        
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        [WarningBox warningBoxHide:YES andView:self.view];
+        [WarningBox warningBoxModeText:@"网络连接失败！" andView:self.view];
+        NSLog(@"错误：%@",error);
+    }];
+    
     
 }
 
@@ -86,23 +153,23 @@
         cell.contentView.backgroundColor = [UIColor whiteColor];
         if (indexPath.section == 0) {
             
-            cell.textLabel.text = [NSString stringWithFormat:@"%@",shang[indexPath.section]];
+            cell.textLabel.text = [NSString stringWithFormat:@"%@",[[data objectForKey:@"office"] objectForKey:@"name"]];
         }
         if (indexPath.section == 1) {
             
-            cell.textLabel.text = [NSString stringWithFormat:@"%@",shang[indexPath.section]];
+            cell.textLabel.text = [NSString stringWithFormat:@"%@",[[data objectForKey:@"office"] objectForKey:@"address"]];
         }
         if (indexPath.section == 2) {
             
-            cell.textLabel.text = [NSString stringWithFormat:@"%@",shang[indexPath.section]];
+            cell.textLabel.text = [NSString stringWithFormat:@"%@",[[data objectForKey:@"office"] objectForKey:@"phone"]];
         }
         if (indexPath.section == 3) {
             
-            cell.textLabel.text = [NSString stringWithFormat:@"%@",shang[indexPath.section]];
+            cell.textLabel.text = [NSString stringWithFormat:@"%@",[[data objectForKey:@"office"] objectForKey:@"master"]];
         }
         if (indexPath.section == 4) {
             
-            cell.textLabel.text = [NSString stringWithFormat:@"%@",shang[indexPath.section]];
+            cell.textLabel.text = [NSString stringWithFormat:@"%@",[[data objectForKey:@"office"] objectForKey:@"remarks"]];
         }
     }
 
