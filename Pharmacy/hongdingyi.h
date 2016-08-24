0bba71f00c0fe4209e1b681b193cf6347722d7c4
xiaolang <langyaoyu@hotmail.com>
2016-08-24 08:54:21 +0800

    又改了一下

--- Pharmacy/hongdingyi.h
+++ Pharmacy/hongdingyi.h
@@ -17,7 +17,7 @@
 //其中斌
 //#define service_host @"http://192.168.1.180:8080"
 //小展
-#define service_host @"http://192.168.1.110:8080"
+#define service_host @"http://192.168.1.103:8080"
 //外网
 //#define service_host @"http://125.211.221.232:60082"
 
